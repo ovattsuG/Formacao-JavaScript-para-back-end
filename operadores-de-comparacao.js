347f4edb943c61e92ee189d83be4a4599dc82ef7
ovattsuG <gustaalves32@gmail.com>
2023-09-12 15:44:53 -0300

    Operadores de comparação

---
+++ operadores-de-comparacao.js
@@ -0,0 +1,27 @@
+//operadores de comparaçao
+//== (comparação implicita)
+
+const numero = 4;
+const texto = "4";
+
+console.log( numero == texto)
+
+//vai retornar true pois é uma comparaçao implicita se fosse uma comparação nao implicita usando === seria false o resultado pois sao tipos de string diferentes
+
+//typeof
+console.log(typeof numero)
+console.log(typeof texto)
+
+//== só compara o valor
+//=== compara o valor e o tipo de dado 
+
+//conversao explicita
+
+Number()
+String()
+
+// Alguns operadores 
+
+// ||:operador "ou", retorna true caso uma condição seja valida;
+// &&:operador "e", retorna true somente se todas as condições forem válidas;
+//!= e !==:operadores "não igual" e "estritamente não igual", utilizados para comparação, da mesma forma que "==" e "===" retornam true ou false.
