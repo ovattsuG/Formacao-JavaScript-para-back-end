5bd05f05b63f4170635250e4d0ec9bd93f601fa7
ovattsuG <gustaalves32@gmail.com>
2023-09-12 15:43:11 -0300

    operadores terarios

---
+++ Operador-ternario.js
@@ -0,0 +1,11 @@
+const idadeMinima = 18;
+const idadeCliente = 15;
+
+if(idadeCliente >= idadeMinima ){
+    console.log("Pode consumir bebida que contém alcool")
+} else{
+    console.log("não pode consumir bebida que contém alcool")
+}
+//Ternário
+                //condição                      True                                        False
+console.log(idadeCliente >= idadeMinima ? "Pode consumir bebida alcoolica" : "não pode consumir bebida alcoolica")
