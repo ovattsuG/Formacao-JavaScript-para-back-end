43b66c4b5c98d6f9f723fd2d1f21e220b14f5fa9
ovattsuG <gustaalves32@gmail.com>
2023-09-13 13:59:47 -0300

    funções e alguns parametros

--- Parametros.js
+++ Parametros.js
@@ -3,3 +3,31 @@
 function soma() {
     return 2 + 2;
 }
+
+//Função sem retorno, com parâmetro
+
+function cumprimentaPessoa(pessoa){
+    console.log(`oi, ${pessoa}!`)
+   }
+   
+   cumprimentaPessoa('Helena')
+
+//Função com retorno, sem parâmetro
+
+   function cumprimentar(){
+    return 'Oi gente!'
+   }
+   
+   function cumprimentaPessoa(nomePessoa) {
+    console.log(`${cumprimentar()} Meu nome é ${nomePessoa}`)
+   }
+   
+   cumprimentaPessoa('Gustavo') // “Oi gente! Meu nome é Gustavo”
+   
+//Função com return e mais de um parâmetro
+
+   function operacaoMatematica(numero1, numero2, numero3) {
+    return numero1 + numero2 + numero3
+   }
+   
+   operacaoMatematica(15, 30, 45) // 90
