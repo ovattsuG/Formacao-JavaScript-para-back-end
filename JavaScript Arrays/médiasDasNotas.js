8880cdc5d39f6d6935b62afd2ca86e3f8eb64f10
ovattsuG <gustaalves32@gmail.com>
2023-10-23 16:58:39 -0300

    Aula-01 de Arrays calculando média

--- JavaScript Arrays/médiasDasNotas.js
+++ JavaScript Arrays/médiasDasNotas.js
@@ -4,11 +4,16 @@
 6,5
 8
 7,5                     */
-
+/* 
 const nota1 = 10;
 const nota2 = 6.5;
 const nota3 = 8;
 const nota4 = 7.5;
 let mediaDasNotas = (nota1 + nota2 + nota3 + nota4)/4;
-console.log(mediaDasNotas);
+console.log(mediaDasNotas); */
 
+/* Utilizando listas */
+const notas = [10,6.5,8,7.5];
+
+let mediaDasNotas = (nota[0] + nota[1] + nota[2] + nota[3])/4;
+console.log(mediaDasNotas);
