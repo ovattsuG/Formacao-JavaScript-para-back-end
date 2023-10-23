26444b1ce8f6fe5c4daed93b4675f2356a5caa83
ovattsuG <gustaalves32@gmail.com>
2023-10-23 16:41:58 -0300

    Aula 01 Arrays - Médias das notas de um aluo

---
+++ JavaScript Arrays/médiasDasNotas.js
@@ -0,0 +1,14 @@
+/* O primeiro desafio será calcular a média das notas de um estudante. Suas notas são:
+
+10
+6,5
+8
+7,5                     */
+
+const nota1 = 10;
+const nota2 = 6.5;
+const nota3 = 8;
+const nota4 = 7.5;
+let mediaDasNotas = (nota1 + nota2 + nota3 + nota4)/4;
+console.log(mediaDasNotas);
+
