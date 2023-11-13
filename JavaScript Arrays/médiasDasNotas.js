9f2f3dc7939be47e63109aed07d336580895867f
ovattsuG <gustaalves32@gmail.com>
2023-11-13 14:18:28 -0300

    Aula sobre introdução a Arrays

--- JavaScript Arrays/médiasDasNotas.js
+++ JavaScript Arrays/médiasDasNotas.js
@@ -12,8 +12,13 @@ const nota4 = 7.5;
 let mediaDasNotas = (nota1 + nota2 + nota3 + nota4)/4;
 console.log(mediaDasNotas); */
 
-/* Utilizando listas */
-const notas = [10,6.5,8,7.5];
 
-let mediaDasNotas = (nota[0] + nota[1] + nota[2] + nota[3])/4;
+/* Utilizando listas(arrays) */
+const notas = [10,6.5,8,7.5]; //<--elementos/itens/valores
+// posição      0  1  2  3
+
+let mediaDasNotas = (nota[0] + nota[1] + nota[2] + nota[3])/ notas.length;
 console.log(mediaDasNotas);
+
+// array é um conjunto de dados
+//length = largura,  define o número de elementos em um determinado array
