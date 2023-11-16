141bfc5a396bb55965d62b6996b6cd6e23f36582
ovattsuG <gustaalves32@gmail.com>
2023-11-16 14:29:03 -0300

    atividade de fixaçao sobre metodos pop e push do js

---
+++ JavaScript Arrays/medoto-pop-e-push.js
@@ -0,0 +1,13 @@
+// O push e pop são usados para adicionar/remover elementos de uma lista, como mostra o seguinte código:
+jantarDeHoje = ['🍔', '🌭', '🍕']
+
+jantarDeHoje.pop()
+jantarDeHoje.pop()
+
+jantarDeHoje.push('🍳')
+jantarDeHoje.push('🥗')
+jantarDeHoje.push('🍏')
+
+console.log(jantarDeHoje)
+
+// retornan "[ '🍔', '🍳', '🥗', '🍏' ]"
